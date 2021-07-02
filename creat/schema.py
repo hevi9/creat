5af759ea798d93f1c3f9ed3edadca5b07dac72ab
Petri Heinilä <hevi00@gmail.com>
2021-07-02 07:09:54 +0300

    refactor: schema

--- creat/schema.py
+++ creat/schema.py
@@ -1,11 +1,49 @@
 """Schema for yaml files."""
 
-from typing import Any, List
+from __future__ import annotations
 
-from pydantic import BaseModel  # pylint: disable=no-name-in-module
+from pathlib import Path
+from typing import Any, List, Optional, Mapping, TypedDict, Union, Sequence
+
+from pydantic import Field
+from pydantic.main import BaseModel
 
 
 class TopLevel(BaseModel):
-    """Top level."""
-
     sources: List[Any]
+
+
+class Item(BaseModel):
+    doc: Optional[str]
+    show: bool
+
+
+class Runnable(Item):
+    cd: Optional[str]
+    env: Optional[Mapping[str, str]]
+
+
+class Action(Runnable):
+    pass
+
+
+class Paths(TypedDict):
+    src: Path
+    dst: Path
+
+
+class Copy(Action):
+    copy_: Union[str, Paths] = Field(alias="copy")
+
+
+class Move(Action):
+    move: Union[str, Paths]
+
+
+class Remove(Action):
+    remove: Union[str, Sequence[Path]]
+
+
+class Source(Runnable):
+    name: str
+    actions: List[Action]
