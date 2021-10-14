213d8d3df008b751169f7eae18639bd139341b34
Petri Heinilä <petri.heinila@visma.com>
2021-10-14 17:25:35 +0300

    fix: non-schema removal fix: shell action

--- creat/action/bases.py
+++ creat/action/bases.py
@@ -14,34 +14,6 @@
 # class Action(Item, Runnable):
 #     """Base implementation for make items."""
 #
-#     _source: Source
-#     _cd: Optional[str]
-#     _env: dict
-#
-#     def __init__(self, source: Source, control: dict):
-#         super().__init__(control, source._location)
-#         self._source = source
-#         self._cd = control.get("cd", None)
-#         self._env = control.get("env", {})
-#
-#     @property
-#     def cd(self) -> Optional[str]:
-#         if self._cd:
-#             return self._cd
-#         if self._source.cd:
-#             return self._source.cd
-#         return None
-#
-#     @property
-#     def env(self) -> Mapping[str, str]:
-#         return dict(self._source.env, **self._env)
-#
-#     def update(self, index: Index) -> None:
-#         pass  # not all runnable need to update
-#
-#     def programs(self) -> Iterable[str]:
-#         return []
-#
 #     @contextmanager
 #     def _run_context(self):
 #         if self.cd:
