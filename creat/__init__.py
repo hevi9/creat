3ddbd6c5b276ed459e7aee3c55220f454c542478
hevi <hevi@example.com>
2024-01-30 21:08:47 +0200

    feat: base typer scaffold structure

---
+++ creat/__init__.py
@@ -0,0 +1,5 @@
+import typer
+
+__version__ = "0.0.0"
+
+app = typer.Typer()
---
+++ creat/__main__.py
@@ -0,0 +1,28 @@
+from typing import Annotated, Optional
+
+import typer
+
+from . import __version__, app
+
+
+def _version() -> None:
+    typer.echo(f"creat {__version__}")
+    raise typer.Exit(0)
+
+
+@app.callback()
+def main(
+    _version: Annotated[
+        Optional[bool],
+        typer.Option(
+            "--version",
+            callback=_version,
+            is_eager=True,
+        ),
+    ] = None,
+) -> None:
+    """."""
+
+
+if __name__ == "__main__":
+    app()
