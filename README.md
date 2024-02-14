736611c965b5176591207e47c8ca3712a70da17d
hevi <hevi@example.com>
2024-02-15 01:21:58 +0200

    feat: config errors

--- README.md
+++ README.md
@@ -7,13 +7,30 @@ Requirements
 * Python 3.10+
 * pipx
 
+Install **creat** to pipx managed virtual environment:
+
 ```shell
+pipx install https://github.com/hevi9/creat.git
 pipx install git+https://git@github.com/hevi9/creat.git
 ```
 
-
 # Run
+
+See command, run and configuration options:
 
 ```shell
 creat --help
 ```
+
+# Develop
+
+Setup local environment:
+
+```shell
+doit local
+```
+
+# Non-transient dependencies
+
+* https://pypi.org/project/copier/
+  * https://copier.readthedocs.io/en/latest/
--- creat/__main__.py
+++ creat/__main__.py
@@ -1,8 +1,11 @@
+import json
+from pathlib import Path
 from typing import Annotated, Optional
 
 import typer
 
 from . import __version__, app
+from .configs import GlobalConfig, set_global_config
 
 
 def _version(value: bool) -> None:
@@ -21,11 +24,23 @@ def main(
             is_eager=True,
         ),
     ] = None,
+    global_config_path: Path = typer.Option(
+        Path("~/.config/creat/creat.json").expanduser(),
+        help="Path to global user config.",
+    ),
 ) -> None:
     """."""
+    try:
+        with open(global_config_path, "r", encoding="utf-8") as fo:
+            data = json.load(fo)
+            config_data = GlobalConfig(**data)
+    except FileNotFoundError:
+        config_data = GlobalConfig()  # type: ignore
+    set_global_config(config_data)
 
 
 from . import sample  # noqa
+from . import config  # noqa
 
 if __name__ == "__main__":
     app()
