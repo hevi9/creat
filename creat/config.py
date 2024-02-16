996b93f025d2754eb2f8dba67942863c6599e0d9
hevi <hevi@example.com>
2024-02-16 23:09:39 +0200

    refactor: config errors

--- creat/config.py
+++ creat/config.py
@@ -1,14 +1,11 @@
-import json
 import sys
 from pathlib import Path
 
 import typer
-from json_source_map import calculate  # type: ignore
-from pydantic_core import ValidationError
 from rich import print
 
 from . import app
-from .configs import LocalConfig
+from .configs import ScaffoldConfig, json_to_obj, ValidationLocationError
 from .configs import get_global_config
 
 
@@ -26,8 +23,8 @@ def _config_local(
 ) -> None:
     """Print current config of default if node defined."""
     config = get_global_config()
-    path = scaffold_path.expanduser() / config.local_config_name
-    text = LocalConfig().model_dump_json(indent=2)
+    path = scaffold_path.expanduser() / config.scaffold_config_name
+    text = ScaffoldConfig().model_dump_json(indent=2)
     if init:
         if not path.exists():
             print(f"Creating local config file at {path.absolute()}")
@@ -43,34 +40,11 @@ def _config_local(
             raise typer.Exit(1)
     if path.exists():
         try:
-            with open(path, "r", encoding="utf-8") as fo:
-                data = json.load(fo)
-                local_config = LocalConfig(**data)
-                print(local_config.model_dump_json(indent=2))
+            obj = json_to_obj(path, ScaffoldConfig)
+            print(obj.model_dump_json(indent=2))
             return
-        except ValidationError as ex:
-            with open(path, "r", encoding="utf-8") as fo:
-                text = fo.read()
-            source_map = calculate(text)
-            for error in ex.errors():
-                # todo implement dict up tracking when exact pointer is not found
-                location_path = list(error["loc"])
-                error_context_path = None
-                while location_path:
-                    try:
-                        pointer = "/" + "/".join([str(i) for i in location_path])
-                        error_context_path = source_map[pointer]
-                        break
-                    except KeyError:
-                        location_path.pop()
-                        continue
-                if error_context_path is not None:
-                    line = error_context_path.value_end.line
-                    column = error_context_path.value_end.column
-                    location = f"{path!s}:{line}:{column}"
-                    subject = error["loc"][-1]
-                    print(location, error["msg"], subject)
-                else:
-                    print(error)
+        except ValidationLocationError as ex:
+            for error in ex.locations:
+                print("ERROR:", error.location, error.msg, f"'{error.subject}'")
             sys.exit(1)
     print(text)
--- creat/configs.py
+++ creat/configs.py
@@ -1,4 +1,10 @@
-from pydantic import BaseModel, Field
+import json
+from pathlib import Path
+from typing import Type, TypeVar
+
+from json_source_map import calculate  # type: ignore
+from pydantic import BaseModel, Field, ConfigDict
+from pydantic_core import ValidationError
 
 
 class ShellRun(BaseModel):
@@ -8,20 +14,23 @@ class ShellRun(BaseModel):
 
 
 class SampleConfig(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
     runs: list[ShellRun] = Field(
         default_factory=list,
         description="""List of runs to execute after scaffold instantiation is made.""",
+        min_items=1,  # type: ignore
     )
 
 
-class LocalConfig(BaseModel):
+class ScaffoldConfig(BaseModel):
     """Project local config."""
 
     sample: SampleConfig = Field(default_factory=SampleConfig)
 
 
 class GlobalConfig(BaseModel):
-    local_config_name: str = Field(
+    scaffold_config_name: str = Field(
         ".creat.json",
         description="Name of project local creat config file",
     )
@@ -41,3 +50,65 @@ def get_global_config() -> GlobalConfig:
     if __global_config is None:
         raise ValueError("Global config not set")
     return __global_config
+
+
+T_Model = TypeVar("T_Model", bound=BaseModel)
+
+
+class ErrorLocation(BaseModel):
+    location: str = ""
+    subject: str = ""
+    msg: str
+    line: int = 0
+    column: int = 0
+
+
+class ValidationLocationError(ValueError):
+    error: ValidationError
+    locations: list[ErrorLocation]
+
+    def __init__(
+        self, msg: str, error: ValidationError, locations: list[ErrorLocation]
+    ) -> None:
+        super().__init__(msg)
+        self.error = error
+        self.locations = locations
+
+
+def json_to_obj(path: Path | str, Model: Type[T_Model]) -> T_Model:
+    try:
+        with open(path, "r", encoding="utf-8") as fo:
+            data = json.load(fo)
+            return Model(**data)
+    # todo JSONDecodeError: Expecting ',' delimiter: line 10 column 1 (char 87)
+    except ValidationError as ex:
+        with open(path, "r", encoding="utf-8") as fo:
+            text = fo.read()
+        source_map = calculate(text)
+        locations = []
+        for error in ex.errors():
+            # todo implement dict up tracking when exact pointer is not found
+            location_path = list(error["loc"])
+            error_context_path = None
+            while location_path:
+                try:
+                    pointer = "/" + "/".join([str(i) for i in location_path])
+                    error_context_path = source_map[pointer]
+                    break
+                except KeyError:
+                    location_path.pop()
+                    continue
+            error_location = ErrorLocation(
+                subject=str(error["loc"][-1]),
+                msg=error["msg"],
+            )
+            if error_context_path is not None:
+                line = error_context_path.value_end.line
+                column = error_context_path.value_end.column
+                error_location.location = f"{path!s}:{line}:{column}"
+                error_location.line = line
+                error_location.column = column
+            locations.append(error_location)
+        raise ValidationLocationError(
+            f"Validation errors on {path!s}", error=ex, locations=locations
+        )
