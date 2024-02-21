3a87b731ec44d68c021c98c884bb35ae78749de3
hevi <hevi@example.com>
2024-02-22 00:29:41 +0200

    feat: list

---
+++ creat/scaffolds.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from .configs import x_user_config
+
+
+class Scaffold:
+    _root: Path
+
+    def __init__(self, root: Path) -> None:
+        self._root = root
+
+    @property
+    def root(self) -> Path:
+        return self._root
+
+    @property
+    def id(self) -> str:
+        return self.root.name
+
+
+def index() -> dict[str, Scaffold]:
+    scaffolds = {}
+    for roots in x_user_config().scaffolds_roots:
+        if roots.is_dir():
+            for root in roots.iterdir():
+                scaffold = Scaffold(root)
+                scaffolds[scaffold.id] = scaffold
+    return scaffolds
