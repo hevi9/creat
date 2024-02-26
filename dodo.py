043c98b53118e4fc155e59e7faeeb8f22045c23a
hevi <hevi@example.com>
2024-02-27 00:25:19 +0200

    feat: doit install

--- dodo.py
+++ dodo.py
@@ -1,8 +1,13 @@
+from pathlib import Path
+
 DOIT_CONFIG = {
     "action_string_formatting": "new",
     "verbosity": 2,
     "default_tasks": ["local"],
 }
+
+dist_root = Path("./dist")
+creat_pkg = dist_root / "creat-0.0.0-py3-none-any.whl"
 
 
 def task_local():
@@ -18,5 +23,19 @@ def task_local():
 def task_check():
     """Check files."""
     return {
-        "actions": ["pre-commit run -a"],
+        "actions": [
+            "pre-commit run -a",
+            "mypy creat",
+        ],
+    }
+
+
+def task_install():
+    """Install creat to user software with pipx."""
+    return {
+        "task_dep": ["check"],
+        "actions": [
+            "poetry build --no-interaction",
+            f"pipx install --force {creat_pkg!s}",
+        ],
     }
