ac74089748ed44c306ee4345ec74c8a44e7439e7
hevi <hevi@example.com>
2024-03-10 18:45:50 +0200

    fix: creat new source target

--- creat/cmd_sample.py
+++ creat/cmd_sample.py
@@ -1,31 +1,38 @@
+import datetime
 from pathlib import Path
 from subprocess import run
 
 import typer
 from copier import Worker
-
-from . import app
-
 from rich import print
-
 from watchfiles import watch
 
-import datetime
+from . import app
+from .processes import cd
+from .scaffolds import build_index
+
+import subprocess
+
+from rich.panel import Panel
 
 
 @app.command("sample")
-def _sample(
-    scaffold_path: Path = typer.Argument(
+def cmd_sample(
+    scaffold_name: str = typer.Argument(
         ...,
-        help="Path to scaffold root to sample.",
-        metavar="PATH",
+        help="Scaffold name to sample.",
+        metavar="SCAFFOLD",
+    ),
+    sample_path: Path = typer.Option(
+        "/tmp/creat/sample",
+        help="Path to the sample scaffold.",
     ),
 ) -> None:
     """Make sample instance from scaffold for development."""
-    sample_path = Path("/tmp/creat/sample")
-    scaffold_path = scaffold_path.expanduser()
+    index = build_index()
+    scaffold = index[scaffold_name]
     worker = Worker(
-        src_path=str(scaffold_path),
+        src_path=str(scaffold.root),
         dst_path=sample_path,
         overwrite=True,
         skip_answered=True,
@@ -33,20 +40,31 @@ def _sample(
 
     def update() -> None:
         if not sample_path.exists():
-            print("new", sample_path, "from", scaffold_path)
+            print("new", sample_path, "from", scaffold.root)
             worker.run_copy()
             run(["git", "-C", str(sample_path), "init"])
             run(["git", "-C", str(sample_path), "add", "."])
             run(["git", "-C", str(sample_path), "commit", "-m", "initial"])
         else:
-            print("recopy", sample_path, "from", scaffold_path)
+            print("recopy", sample_path, "from", scaffold.root)
             worker.run_recopy()
+
+        if scaffold.config.sample.runs:
+            print(f"Run sample {sample_path} runs:")
+            with cd(sample_path):
+                for irun in scaffold.config.sample.runs:
+                    try:
+                        print(f"Run: '{irun.text}'")
+                        subprocess.run(irun.text, shell=True, check=True)
+                    except subprocess.CalledProcessError as ex:
+                        print(Panel(f"{ex!s}", title="ERROR"))
+
         dt = datetime.datetime.now()
-        print(f"{dt.strftime('%Y%m%dT%H%M%S')} {sample_path} updated")
+        print(f"{dt.strftime('%Y-%m-%d %H:%M:%S')} Sample target {sample_path} updated")
 
     update()
 
-    print(f"Watching changes from {scaffold_path!s} ..")
-    for changes in watch(scaffold_path, debounce=5000):
-        print(changes)
+    print(f"Watching changes from {scaffold.root!s} ..")
+    for changes in watch(scaffold.root, debounce=5000):
+        print("changes:", changes)
         update()
